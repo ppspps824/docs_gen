fc804fa1ac80816570d31631ae14c56f3fe43786
papasim824 <example@example.com>
2023-06-18 16:47:19 +0900

    mod

--- docs_gen.py
+++ docs_gen.py
@@ -659,7 +659,7 @@ if __name__ == "__main__":
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-    os.environ["OPENAI_API_KEY"] = st.secrets["OPEN_AI_KEY"]
+    openai.api_key = st.secrets["OPEN_AI_KEY"]
     # os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
     # os.environ["GOOGLE_CSE_ID"] = st.secrets["GOOGLE_CSE_ID"]
 
